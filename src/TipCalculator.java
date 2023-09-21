67ced24e23cbcfd2304847696d5c4c24c75d8c57
BT_2N2_34 <sample@bths.edu>
2023-09-21 13:20:08 -0400

    Started the code

---
+++ src/TipCalculator.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class TipCalculator {
+    public static void main(String[] args)  {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to the tip calculator");
+        System.out.print("How many people are in your group: ");
+        int people = scan.nextInt();
+        scan.nextLine();
+
+    }
+}
